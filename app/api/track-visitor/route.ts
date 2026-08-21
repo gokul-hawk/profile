99b288ccd9b56da694ff41ddb41a5f3baa376864
hawkiee <gokulaudhayannv@gmail.com>
2026-08-21 16:15:46 +0530

    adjustment to about

--- app/api/track-visitor/route.ts
+++ app/api/track-visitor/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { PrismaClient } from "@/src/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg({ connectionString: process.env.POSTGRES_PRISMA_URL_PRISMA_DATABASE_URL });
 const prisma = new PrismaClient({ adapter });
 
 const TIMEOUT_MS = 4000;
